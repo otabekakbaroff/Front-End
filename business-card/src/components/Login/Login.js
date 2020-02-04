3da434ea14762e73465e279817e6b09d55b098be
LuisR <luigirodriguez21@yahoo.com>
2020-02-04 11:00:58 -0600

    done with Login

--- business-card/src/components/Login/Login.js
+++ business-card/src/components/Login/Login.js
@@ -1,9 +1,54 @@
-import React from 'react';
+import React from "react";
+import { Formik } from "formik";
 
+const Login = () => (
+  <Formik
+    initialValues={{ email: "", password: "" }}
+    onSubmit={(values, { setSubmitting }) => {
+      setTimeout(() => {
+        console.log("Logging in", values);
+        setSubmitting(false);
+      }, 500);
+    }}>
+    {props => {
+      const {values, touched, errors, isSubmitting, handleChange, handleBlur, handleSubmit} = props;
+      return (
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="email">Email</label>
+          <input
+            name="email"
+            type="text"
+            placeholder="Enter your email"
+            value={values.email}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            className={errors.email && touched.email && "error"}
+          />
+          {errors.email && touched.email && (
+            <div className="input-feedback">{errors.email}</div>
+          )}
+          <br></br>
+          <label htmlFor="email">Password</label>
+          <input
+            name="password"
+            type="password"
+            placeholder="Enter your password"
+            value={values.password}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            className={errors.password && touched.password && "error"}
+          />
+          {errors.password && touched.password && (
+            <div className="input-feedback">{errors.password}</div>
+          )}
+          <br></br>
+          <button type="submit" disabled={isSubmitting}>
+            Login
+          </button>
+        </form>
+      );
+    }}
+  </Formik>
+);
 
-export default function Login(){
-    return(
-        <h1>Login</h1>
-    )
-}
-
+export default Login;
